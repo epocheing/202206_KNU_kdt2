8391318dc9c565c9d9fcfb7885c20be07168716b
Inuk Song <71516845+SsongIU@users.noreply.github.com>
2022-08-05 21:13:18 +0900

    Update mycode.R

--- week_1/D_0804/mycode.R
+++ week_1/D_0804/mycode.R
@@ -1,23 +1,23 @@
 # r 변수 설정
-3+4
-print('hello world')
+3 + 4
+print("hello world")
 
 x <- 3
 5 -> y
-x+y
+x + y
 
 x <- 5
 y <- 10
-z <- x+y
+z <- x + y
 
 # r 라이브러리 설치
 library(cowsay)
-install.packages('cowsay')
+install.packages("cowsay")
 
-say('Hello, World!')
-say('Hello, Chicken', by = 'chicken')
+say("Hello, World!")
+say("Hello, Chicken", by = "chicken")
 
-install.packages('ggplot2')
+install.packages("ggplot2")
 
 getwd()
 
@@ -40,29 +40,28 @@ table(iris$Species)
 
 barplot(table(iris$Species))
 
-hist(iris$Petal.Length, col = 'steelblue',
-     main = '꽃잎의 길이에 대한 히스토그램',
-     xlab = '꽃잎의 길이',
-     ylab = '빈도수')
+hist(iris$Petal.Length,
+     col = "steelblue",
+     main = "꽃잎의 길이에 대한 히스토그램",
+     xlab = "꽃잎의 길이",
+     ylab = "빈도수"
+)
 
-plot(mtcars$mpg, mtcars$wt, col='tomato')
+plot(mtcars$mpg, mtcars$wt, col = "tomato")
 
 str(cars)
 ?cars
 
 plot(cars$speed, cars$dist,
-     col = 'tomato',
-     pch = 19)
+     col = "tomato",
+     pch = 19
+)
 
 str(iris)
 mean(iris$Petal.Length)
 var(iris$Petal.Length)
 sd(iris$Petal.Length)
-hist(iris$Petal.Length, col="tomato",
-     breaks = 5)
-
-
-
-
-
-
+hist(iris$Petal.Length,
+     col = "tomato",
+     breaks = 5
+)
