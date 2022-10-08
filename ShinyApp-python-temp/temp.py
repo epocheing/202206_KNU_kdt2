74f77ec3fc7a340edca6d41451db37feab303415
Inuk Song <71516845+SsongIU@users.noreply.github.com>
2022-10-08 15:09:05 +0900

    update temp.py

--- ShinyApp-python-temp/temp.py
+++ ShinyApp-python-temp/temp.py
@@ -1,34 +1,55 @@
 from shiny import App, render, ui
 from shiny.types import ImgData
+from pathlib import Path
 
 app_ui = ui.page_fluid(
     ui.panel_title("적조 알라미 ver1.0"),
     ui.panel_sidebar(
-        ui.input_selectize(
-            id="image",
-            label="매뉴",
-            choices={
-                "hab.png": "적조 예측",
-                "gps.png": "위치 정보",
-                "map.png": "적조 정보",
-                "weather.png": "날씨 정보",
-                "tour.png": "관광 정보",
-            },
+        ui.navset_tab(
+            ui.nav("적조 예측", ui.output_image("hab")),
+            ui.nav("위치 정보", ui.output_image("gps")),
+            ui.nav("적조 정보", ui.output_image("map")),
+            ui.nav("날씨 정보", ui.output_image("weather")),
+            ui.nav("관광 정보", ui.output_image("tour")),
         )
     ),
-    ui.panel_main(ui.output_image("img")),
 )
 
 
 def server(input, output, session):
     @output
     @render.image
-    def img():
-        from pathlib import Path
-
+    def hab():
         dir = Path(__file__).resolve().parent
-        name = str(input.image())
-        img: ImgData = {"src": str(dir / "image/" / name), "width": "600px"}
+        img: ImgData = {"src": str(dir / "image/hab.png"), "width": "600px"}
+        return img
+
+    @output
+    @render.image
+    def gps():
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "image/gps.png"), "width": "600px"}
+        return img
+
+    @output
+    @render.image
+    def map():
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "image/map.png"), "width": "600px"}
+        return img
+
+    @output
+    @render.image
+    def weather():
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "image/weather.png"), "width": "600px"}
+        return img
+
+    @output
+    @render.image
+    def tour():
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "image/tour.png"), "width": "600px"}
         return img
 
 
